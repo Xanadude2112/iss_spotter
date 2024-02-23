7a21842fc3ecc5c68a6f5117b273a9fe81022687
Thomas Azran <tommy.azran@gmail.com>
2024-02-23 22:57:06 +0000

    add helpful comments

--- iss.js
+++ iss.js
@@ -26,7 +26,18 @@ const fetchMyIP = (callback) => {
   });
 };
 
+
+// /**
+//  * Fetches geographical coordinates (latitude and longitude) based on the provided IP address.
+//  * @param {string} ip - The IP address for which to fetch coordinates.
+//  * @param {function} callback - A callback function to handle the asynchronous response.
+//  */
 const fetchCoordsByIP = (ip, callback) => {
+//   /**
+//  * Sends a request to retrieve detailed information about the provided IP address from the ipwho.is API.
+//  * @param {string} ip - The IP address for which to retrieve information.
+//  * @param {function} callback - A callback function to handle the asynchronous response.
+//  */
   request(`http://ipwho.is/${ip}`, (error, response, body) => {
     if (error) {
       return callback(error, null);
@@ -36,7 +47,9 @@ const fetchCoordsByIP = (ip, callback) => {
     const latitude = parseContent.latitude;
     const longitude = parseContent.longitude;
 
+    // Checks if the parsing operation was unsuccessful by verifying the value of the 'success' property in the parsed content object.
     if (!parseContent.success) {
+      // Constructs a message string that includes details about the success status, server message, and the IP address for which the request was made.
       const message = `Success status was ${parseContent.success}. Server message says: ${parseContent.message} when fetching for IP ${parseContent.ip}`;
       callback(Error(message), null);
       return;
