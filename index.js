627d1448d86d90e98ac7dc7b76e1de81c78e89fe
Thomas Azran <tommy.azran@gmail.com>
2024-02-24 13:40:15 +0000

    finish iss 3

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 // const { fetchMyIP } = require('./iss');
 // const { fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const exampleCoor = { latitude: '45.5016889', longitude: '-73.567256' };
+
+// fetchISSFlyOverTimes(exampleCoor, (error, passing) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+    
+//   console.log('It worked! Returned:', passing);
+// });
 
 // fetchCoordsByIP('192.222.192.214', (error, coordinates) => {
 //   if (error) {
--- iss.js
+++ iss.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const request = require("request");
 /**
  * Makes a single API request to retrieve the user's IP address.
  * Input:
@@ -8,9 +8,9 @@ const request = require('request');
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 const fetchMyIP = (callback) => {
-  request('http://ipwho.is/8.8.4.4', (error, response, body) => {
-  // inside the request callback ...
-  // error can be set if invalid domain, user is offline, etc.
+  request("http://ipwho.is/8.8.4.4", (error, response, body) => {
+    // inside the request callback ...
+    // error can be set if invalid domain, user is offline, etc.
     if (error) {
       return callback(error, null);
     }
@@ -26,18 +26,17 @@ const fetchMyIP = (callback) => {
   });
 };
 
-
 // /**
 //  * Fetches geographical coordinates (latitude and longitude) based on the provided IP address.
 //  * @param {string} ip - The IP address for which to fetch coordinates.
 //  * @param {function} callback - A callback function to handle the asynchronous response.
 //  */
 const fetchCoordsByIP = (ip, callback) => {
-//   /**
-//  * Sends a request to retrieve detailed information about the provided IP address from the ipwho.is API.
-//  * @param {string} ip - The IP address for which to retrieve information.
-//  * @param {function} callback - A callback function to handle the asynchronous response.
-//  */
+  //   /**
+  //  * Sends a request to retrieve detailed information about the provided IP address from the ipwho.is API.
+  //  * @param {string} ip - The IP address for which to retrieve information.
+  //  * @param {function} callback - A callback function to handle the asynchronous response.
+  //  */
   request(`http://ipwho.is/${ip}`, (error, response, body) => {
     if (error) {
       return callback(error, null);
@@ -55,8 +54,39 @@ const fetchCoordsByIP = (ip, callback) => {
       return;
     }
 
-    callback(null, {latitude, longitude});
+    callback(null, { latitude, longitude });
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP};
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+  // Sends a request to the specified URL, which provides JSON data related to the International Space Station (ISS) flyover times.
+  request(
+    `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`,
+    (error, response, body) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      if (response.statusCode !== 200) {
+        callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+        return;
+      }
+
+      const parseContent = JSON.parse(body).response;
+
+      callback(null, parseContent);
+    }
+  );
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
